63f234ff6b99ed3c8bc549c7ddd490d0fb4a4851
jarbasal <jarbasai@mailfence.com>
2021-10-28 21:59:13 +0100

    :tada:

---
+++ README.md
@@ -0,0 +1,49 @@
+# <img src='./res/icon/documentaries_icon.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/>r/DocumentariesSkill
+
+![](./res/documentaries_logo.png)
+
+## About 
+
+Documentaries from r/Documentaries
+
+![](./gui2.png)
+![](./gui.png)
+
+## Examples 
+
+* "play a documentary"
+
+# Platform support
+
+- :heavy_check_mark: - tested and confirmed working
+- :x: - incompatible/non-functional
+- :question: - untested
+- :construction: - partial support
+
+|     platform    |   status   |  tag  | version | last tested | 
+|:---------------:|:----------:|:-----:|:-------:|:-----------:|
+|    [Chatterbox](https://hellochatterbox.com)   | :question: |  dev  |         |    never    | 
+|     [HolmesV](https://github.com/HelloChatterbox/HolmesV)     | :question: |  dev  |         |    never    | 
+|    [LocalHive](https://github.com/JarbasHiveMind/LocalHive)    | :question: |  dev  |         |    never    |  
+|  [Mycroft Mark1](https://github.com/MycroftAI/enclosure-mark1)    | :question: |  dev  |         |    never    | 
+|  [Mycroft Mark2](https://github.com/MycroftAI/hardware-mycroft-mark-II)    | :question: |  dev  |         |    never    |  
+|    [NeonGecko](https://neon.ai)      | :question: |  dev  |         |    never    |   
+|       [OVOS](https://github.com/OpenVoiceOS)        | :question: |  dev  |         |    never    |    
+|     [Picroft](https://github.com/MycroftAI/enclosure-picroft)       | :question: |  dev  |         |    never    |  
+| [Plasma Bigscreen](https://plasma-bigscreen.org/)  | :question: |  dev  |         |    never    |  
+
+- `tag` - link to github release / branch / commit
+- `version` - link to release/commit of platform repo where this was tested
+
+
+## Credits 
+- JarbasAl
+- [r/Documentaries](https://www.reddit.com/r/Documentaries/)
+
+## Category
+**Entertainment**
+
+## Tags
+#documentaries
+#educational
+#entertainment
---
+++ __init__.py
@@ -0,0 +1,78 @@
+from os.path import join, dirname
+
+import bs4
+from ovos_plugin_common_play.ocp import MediaType, PlaybackType
+from ovos_utils.parse import fuzzy_match, MatchStrategy
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
+    ocp_search
+from reddit_movies import RedditDocumentaries
+
+
+class DocumentariesSkill(OVOSCommonPlaybackSkill):
+
+    def __init__(self):
+        super().__init__("Documentaries")
+        self.skill_icon = join(dirname(__file__), "ui",
+                               "documentaries_icon.png")
+        self.media_type = MediaType.DOCUMENTARY
+        self.playback_type = PlaybackType.AUDIO
+        self.supported_media = [MediaType.DOCUMENTARY,
+                                MediaType.GENERIC,
+                                MediaType.VIDEO]
+
+        # get your own keys! these might stop working any time
+        if "praw_client" not in self.settings:
+            self.settings["praw_client"] = "Cij47m8Dg6dSIA"
+        if "praw_secret" not in self.settings:
+            self.settings["praw_secret"] = "7Hib7ujbWsrmzvgw93woBQ923w0"
+
+    def calc_score(self, phrase, match, base_score=0):
+        # check for forbidden words in title
+        if self.voc_match(match["title"].lower(), "blacklist"):
+            return 0
+
+        score = 100 * fuzzy_match(phrase.lower(), match["title"].lower(),
+                                  strategy=MatchStrategy.TOKEN_SET_RATIO)
+        return min(100, base_score + score)
+
+    def parse_media_type(self, phrase, media_type):
+        score = 0
+        if self.voc_match(phrase, "reddit"):
+            score += 10
+            phrase = self.remove_voc(phrase, "reddit")
+        if self.voc_match(phrase, "documentaries") or \
+                media_type == MediaType.DOCUMENTARY:
+            score += 50
+            phrase = self.remove_voc(phrase, "documentaries")
+        return score, phrase
+
+    @ocp_search()
+    def search_reddit(self, phrase, media_type):
+        base_score, phrase = self.parse_media_type(phrase, media_type)
+        if self.settings.get("praw_secret") and self.settings.get("praw_client"):
+            r = RedditDocumentaries(client=self.settings["praw_client"],
+                                    secret=self.settings["praw_secret"])
+        else:
+            r = RedditDocumentaries()
+
+        for v in r.scrap():
+            score = self.calc_score(phrase, v, base_score=base_score)
+            if score < 20:
+                continue
+            # return as a video result (single track dict)
+            yield {
+                "match_confidence": score,
+                "media_type": MediaType.DOCUMENTARY,
+                #  "length": v.length * 1000,
+                "uri": "youtube//" + v["url"],
+                "playback": PlaybackType.VIDEO,
+                "image": v.get("thumbnail") or self.skill_icon,
+                "bg_image": v.get("thumbnail") or self.skill_icon,
+                "skill_icon": self.skill_icon,
+                "title": v["title"],
+                "skill_id": self.skill_id
+            }
+
+
+def create_skill():
+    return DocumentariesSkill()
