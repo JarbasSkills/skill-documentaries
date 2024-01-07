230a3ab6b42f9d8e327a43f97d1752bac9c6e2a7
JarbasAi <jarbasai@mailfence.com>
2024-01-07 04:56:13 +0000

    refactor/ocp

--- __init__.py
+++ __init__.py
@@ -1,78 +1,124 @@
 from os.path import join, dirname
 
-import bs4
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_utils.parse import fuzzy_match, MatchStrategy
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
-    ocp_search
+from reddit_movies import RedditMovies
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
+
+
 from reddit_movies import RedditDocumentaries
 
 
 class DocumentariesSkill(OVOSCommonPlaybackSkill):
-
-    def __init__(self):
-        super().__init__("Documentaries")
+    def __init__(self, *args, **kwargs):
+        self.supported_media = [MediaType.DOCUMENTARY]
         self.skill_icon = join(dirname(__file__), "ui",
                                "documentaries_icon.png")
-        self.media_type = MediaType.DOCUMENTARY
-        self.playback_type = PlaybackType.AUDIO
-        self.supported_media = [MediaType.DOCUMENTARY,
-                                MediaType.GENERIC,
-                                MediaType.VIDEO]
+        super().__init__(*args, **kwargs)
 
+    def initialize(self):
         # get your own keys! these might stop working any time
         if "praw_client" not in self.settings:
             self.settings["praw_client"] = "Cij47m8Dg6dSIA"
         if "praw_secret" not in self.settings:
             self.settings["praw_secret"] = "7Hib7ujbWsrmzvgw93woBQ923w0"
 
-    def calc_score(self, phrase, match, base_score=0):
-        # check for forbidden words in title
-        if self.voc_match(match["title"].lower(), "blacklist"):
-            return 0
+        if self.settings.get("praw_secret") and \
+                self.settings.get("praw_client"):
+            self.reddit = RedditDocumentaries(
+                client=self.settings["praw_client"],
+                secret=self.settings["praw_secret"])
 
-        score = 100 * fuzzy_match(phrase.lower(), match["title"].lower(),
-                                  strategy=MatchStrategy.TOKEN_SET_RATIO)
-        return min(100, base_score + score)
+        self._scrap_reddit()
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_streaming_provider",
+                                  ["Reddit Documentaries", "Reddit"])
 
-    def parse_media_type(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "reddit"):
-            score += 10
-            phrase = self.remove_voc(phrase, "reddit")
-        if self.voc_match(phrase, "documentaries") or \
-                media_type == MediaType.DOCUMENTARY:
-            score += 50
-            phrase = self.remove_voc(phrase, "documentaries")
-        return score, phrase
+    def _scrap_reddit(self, message=None):
+        movies = []
+        for v in self.reddit.get_cached_entries():
+            t = v["title"].split(" (")[0].replace("COMPLETE", "")
+            if '"' in t:
+                t = t.split('"')[1]
+            movies.append(t.strip())
+        try:
+            for v in self.reddit.scrap():
+                print(v)
+                t = v["title"].split(" (")[0].replace("COMPLETE", "")
+                if '"' in t:
+                    t = t.split('"')[1]
+                movies.append(t.strip())
+        except:
+            pass
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_name", movies)
+        self.schedule_event(self._scrap_reddit, 3600)  # repeat every hour
 
     @ocp_search()
     def search_reddit(self, phrase, media_type):
-        base_score, phrase = self.parse_media_type(phrase, media_type)
-        if self.settings.get("praw_secret") and self.settings.get("praw_client"):
-            r = RedditDocumentaries(client=self.settings["praw_client"],
-                                    secret=self.settings["praw_secret"])
-        else:
-            r = RedditDocumentaries()
+        base_score = 15 if media_type == MediaType.DOCUMENTARY else 0
+        entities = self.ocp_voc_match(phrase)
 
-        for v in r.scrap():
-            score = self.calc_score(phrase, v, base_score=base_score)
-            if score < 20:
-                continue
-            # return as a video result (single track dict)
-            yield {
-                "match_confidence": score,
-                "media_type": MediaType.DOCUMENTARY,
-                #  "length": v.length * 1000,
-                "uri": "youtube//" + v["url"],
-                "playback": PlaybackType.VIDEO,
-                "image": v.get("thumbnail") or self.skill_icon,
-                "bg_image": v.get("thumbnail") or self.skill_icon,
-                "skill_icon": self.skill_icon,
-                "title": v["title"],
-                "skill_id": self.skill_id
-            }
+        title = entities.get("documentary_name")
+        skill = "documentary_streaming_provider" in entities  # skill matched
+
+        base_score += 30 * len(entities)
+        if title:
+            base_score += 30
+            # search cached database (updated hourly)
+            for v in self.reddit.get_cached_entries():
+                if title.lower() in v["title"].lower():
+                    # return as a video result (single track dict)
+                    yield {
+                        "match_confidence": base_score,
+                        "media_type": MediaType.DOCUMENTARY,
+                        #  "length": v.length * 1000,
+                        "uri": "youtube//" + v["url"],
+                        "playback": PlaybackType.VIDEO,
+                        "image": v.get("thumbnail") or self.skill_icon,
+                        "bg_image": v.get("thumbnail") or self.skill_icon,
+                        "skill_icon": self.skill_icon,
+                        "title": v["title"],
+                        "skill_id": self.skill_id
+                    }
+        if skill:
+            yield self.get_playlist()
+
+    def get_playlist(self, score=50, num_entries=25):
+        pl = self.featured_media()[:num_entries]
+        return {
+            "match_confidence": score,
+            "media_type": MediaType.DOCUMENTARY,
+            "playlist": pl,
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "image": self.skill_icon,
+            "title": "Reddit Documentaries (Documentary Playlist)",
+            "author": "Reddit"
+        }
+
+    @ocp_featured_media()
+    def featured_media(self):
+        return [{
+            "match_confidence": 50,
+            "media_type": MediaType.DOCUMENTARY,
+            #  "length": v.length * 1000,
+            "uri": "youtube//" + v["url"],
+            "playback": PlaybackType.VIDEO,
+            "image": v.get("thumbnail") or self.skill_icon,
+            "bg_image": v.get("thumbnail") or self.skill_icon,
+            "skill_icon": self.skill_icon,
+            "title": v["title"],
+            "skill_id": self.skill_id
+        } for v in self.reddit.get_cached_entries()]
 
 
-def create_skill():
-    return DocumentariesSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = DocumentariesSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_reddit("Vatican City", MediaType.DOCUMENTARY):
+        print(r)
+        # {'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'youtube//https://www.youtube.com/watch?v=gwrKLL4Gefg', 'playback': <PlaybackType.VIDEO: 1>, 'image': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'bg_image': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'title': 'Vatican City', 'skill_id': 't.fake'}
+
